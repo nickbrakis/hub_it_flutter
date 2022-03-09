ec884bb4ac37975624448a82ad7963abfe6a8744
nickbrakis <nibrakis@gmail.com>
2022-03-09 20:02:40 +0200

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(HubIt());
+
+
+class HubIt extends StatelessWidget {
+  const HubIt({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hub.it Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: Iphone13promaxhomeWidget (),
+    );
+  }
+}
+
+class Iphone13promaxhomeWidget  extends StatefulWidget{
+  const Iphone13promaxhomeWidget ({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _Iphone13promaxhomeWidget ();
+}
+
+class _Iphone13promaxhomeWidget  extends State<Iphone13promaxhomeWidget >{
+  int _selectedIndex = 0;
+  
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            label: 'Hubs',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white,
+        onTap: _onItemTapped,
+        backgroundColor: Color.fromARGB(255, 49, 49, 49),
+      )
+    );
+  }
+}

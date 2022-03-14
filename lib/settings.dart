81f58069eb2a76dc36be60ad32b6bb7f3a4ed2b3
Tom Spyropoulos <tomody.spy@gmail.com>
2022-03-14 13:28:29 +0200

    added bottomnavbar functionality and hubs/settings dummy pages

---
+++ lib/settings.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SettingsWidget extends StatefulWidget {
+  const SettingsWidget({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _SettingsWidgetState();
+}
+
+class _SettingsWidgetState extends State<SettingsWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Settings Page"),
+      ),
+    );
+  }
+}

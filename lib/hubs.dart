98ef5345af04bdd458e2fca73275b83db9086d79
nickbrakis <nibrakis@gmail.com>
2022-03-15 19:55:29 +0200

    Hubs body List ready except follow buttons

--- lib/hubs.dart
+++ lib/hubs.dart
@@ -32,6 +32,55 @@ class _HubsWidgetState extends State<HubsWidget> {
     });
   }
 
+  Widget _hubsBodyList() {
+    destroyHubsList();
+    buildHubsList();
+
+    return Container(
+      alignment: Alignment.center,
+      width : double.infinity,
+      height: 500,
+      child : Container (
+        width : 358,
+        height : 457,
+        margin: const EdgeInsets.all(10.0),
+        color: const Color.fromARGB(255, 165, 165, 165),
+        child : ListView.separated(
+          shrinkWrap: true,
+          itemCount: _hubs.length + 1,
+          separatorBuilder:  (context, index) => const Divider(),
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: index == 0 ? null : const Icon(Icons.accessibility_sharp),
+              title : index == 0 ? const Text("My Hubs", style : TextStyle(fontSize : 25)) :Text(_hubs[index - 1].title),
+              trailing :index == 0 ? null : Container (
+                width : 75,
+                height :32,
+                decoration: const BoxDecoration( 
+                  color: Color.fromARGB(255, 204, 77, 77),
+                  borderRadius: BorderRadius.all(Radius.circular(20))
+                ),
+                child : IconButton(
+                  icon: const Text('Unfollow', 
+                    textAlign: TextAlign.center, 
+                    style: TextStyle(
+                      color: Color.fromRGBO(0, 0, 0, 1),
+                      fontFamily: 'Roboto',
+                      fontSize: 14,
+                      letterSpacing: 0.25,
+                      fontWeight: FontWeight.normal,
+                      )
+                    ),
+                  onPressed: () {},
+                ),
+              )
+            );
+          }, 
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,6 +98,7 @@ class _HubsWidgetState extends State<HubsWidget> {
           )
         ],
       ),
+      body : _hubsBodyList(),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
@@ -75,6 +125,7 @@ class _HubsWidgetState extends State<HubsWidget> {
 }
 
 class MySearchDelegate extends SearchDelegate { 
+
   List <String> suggestions = [
       'Photography',
       'Cinema', 
@@ -90,6 +141,9 @@ class MySearchDelegate extends SearchDelegate {
     }
     return Center(child: Text(query));
   }
+
+  @override
+  final searchFieldLabel = "find hubs!";
 
   @override
   Widget? buildLeading(BuildContext context) => IconButton(
@@ -131,17 +185,17 @@ class MySearchDelegate extends SearchDelegate {
     return ListView.builder(
       itemCount: listToShow.length,
       itemBuilder: (context, index) {
-        var res_suggestion = listToShow[index];
+        var resSuggestion = listToShow[index];
         return ListTile(
-          title: Text(res_suggestion),
+          title: Text(resSuggestion),
           onTap: () {
-            query = res_suggestion;
+            query = resSuggestion;
             showResults(context);
           },
           trailing : IconButton (
             icon:const Icon(Icons.add),
             onPressed: () {
-              query = res_suggestion;
+              query = resSuggestion;
               showResults(context);
             },
           ),
@@ -150,4 +204,35 @@ class MySearchDelegate extends SearchDelegate {
     );
 
   }
+}
+
+// Create some dummy data
+// Hubs data struct : Obj Hubs [String title,List <String> habbits]
+final _hubs = <Hub>[];
+final hub0 =
+    Hub(title: "MyHub", enabled: true, habbits: ["Work", "Study", "Cook"]);
+final hub1 = Hub(
+    title: "German Class",
+    enabled: true,
+    habbits: ["Studying", "Listening", "Videos"]);
+final hub2 = Hub(
+    title: "Gym Freaks",
+    enabled: true,
+    habbits: ["Workout", "Pre Meal", "After meal"]);
+void buildHubsList() {
+  _hubs.add(hub0);
+  _hubs.add(hub1);
+  _hubs.add(hub2);
+}
+
+void destroyHubsList() {
+  _hubs.clear();
+}
+
+class Hub {
+  String title;
+  List<String> habbits;
+  bool enabled;
+
+  Hub({required this.title, required this.habbits, required this.enabled});
 }

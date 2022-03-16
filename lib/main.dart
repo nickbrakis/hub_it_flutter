5db1f05920a18aa18ea5ca6cf0262e6f910bb8d3
Yiannos <paranomosyiannos@gmail.com>
2022-03-16 21:20:51 +0200

    homepage updt

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:expandable/expandable.dart';
-import 'package:flutter/material.dart';
-// import 'package:play/task.dart';
-import 'assets/hubTile.dart';
 
+import "package:flutter/material.dart";
+import "screens/homeScreen.dart";
 void main() => runApp(const Hubit());
 
 class Hubit extends StatelessWidget {
@@ -15,60 +13,7 @@ class Hubit extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-    home: const HubitScreenWidget(),
-    );
-  }
-}
-
-class HubitScreenWidget extends StatefulWidget{
-  const HubitScreenWidget({Key? key}) : super(key: key);
-
-  @override
-  State<StatefulWidget> createState() => _HubitScreenWidget();
-}
-
-class _HubitScreenWidget extends State<HubitScreenWidget>{ //actually starting here
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Hub.it!'),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_drop_down_sharp),
-          onPressed: (){},
-          tooltip: 'Calendar',
-        ),
-      ),
-      body: const HubsList(),
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            IconButton(onPressed: () {}, icon: const Icon(null)),
-          ],
-        ),
-        color: Colors.blue,
-      ),
-    );
-  }
-}
-
-class HubsList extends StatefulWidget {
-  const HubsList({Key? key}) : super(key: key);
-
-  @override
-  State<HubsList> createState() => _HubsList();
-}
-
-class _HubsList extends State<HubsList> {
-  final List<Widget> _data = generateItems(20);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      padding: const EdgeInsets.all(8),
-      children:  _data
+    home: const HubitHomeScreen(),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,13 +8,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hub_it_app/main.dart';
-import 'package:hub_it_app/main.dart';
+import 'package:hub_it_app/main(seperated).dart';
+import 'package:hub_it_app/main(seperated).dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(HubIt());
+    await tester.pumpWidget(const Hubit());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
